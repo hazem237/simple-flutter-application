d2e73cac125dfdbb2bfea25a8c3fe1e66139cde5
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-10-04 11:45:10 +0300

    add myApp function to build the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Student List'),
+        ),
+        body: StudentList(),
+      ),
+    );
+  }
+}
+
+class StudentList extends StatelessWidget {
+  final List<Student> students = [];
+
+  StudentList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: students.length,
+      itemBuilder: (context, index) {
+        return StudentItem(student: students[index]);
+      },
+    );
+  }
+}
+
 class Student {
   final int id;
   final String name;
