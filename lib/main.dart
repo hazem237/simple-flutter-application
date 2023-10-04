93e533449e1ae7089438b1350a7d1bf54c18b4a4
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-10-04 11:37:36 +0300

    create StudentItem widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class Student {
   final String name;
   final String major;
   final double average;
-  final String imagePath; 
+  final String imagePath;
 
   Student({
     required this.id,
@@ -17,4 +17,31 @@ class Student {
   });
 }
 
+class StudentItem extends StatelessWidget {
+  final Student student;
 
+  const StudentItem({super.key, required this.student});
+
+  @override
+  Widget build(BuildContext context) {
+    double rating = student.average / 20.0;
+
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: AssetImage(student.imagePath),
+      ),
+      title: Text(student.name),
+      subtitle: Text(student.major),
+      trailing: RatingBarIndicator(
+        rating: rating,
+        itemBuilder: (context, _) => const Icon(
+          Icons.star,
+          color: Colors.amber,
+        ),
+        itemCount: 5,
+        itemSize: 24.0,
+        unratedColor: Colors.grey[300],
+      ),
+    );
+  }
+}
